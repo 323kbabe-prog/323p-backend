736a47961624120f56fc55bf9e9ac12936c6dd5f
323kbabe-prog <323kbabe@gmail.com>
2025-10-15 20:23:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ async function generateImageUrl(brand, product, persona, topic = "cosmetics") {
       promptText = `
 Create a futuristic AI product reveal photocard.
 Product name: ${product} by ${brand}.
-Concept: Create a system flowchart only use emojis.
+Concept: Create a system flowchart use emojis with detial text.
 Include small clean label text near bottom: "1ai323.ai 🌐🤖".
 No humans or faces. Focus on product design only.
 `;
