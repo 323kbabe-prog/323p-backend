383efab255a28768f303804fd01c1bbeed3e3863
323kbabe-prog <323kbabe@gmail.com>
2026-04-25 11:11:40 +0800

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,24 +1,76 @@
-getStats() {
+// analytics.js
 
-  let totalJoins = 0;
-  let totalMessages = 0;
-  let totalPresence = 0;
+function createAnalytics() {
 
-  for (const userId in store) {
-    const u = store[userId];
+  const store = {};
 
-    totalJoins += u.joins || 0;
-    totalMessages += u.messages || 0;
-    totalPresence += u.presenceReplies || 0;
+  function ensure(userId) {
+    if (!store[userId]) {
+      store[userId] = {
+        joins: 0,
+        messages: 0,
+        presenceReplies: 0,
+        rooms: {},
+        lastActive: Date.now(),
+        createdAt: Date.now(),
+        disconnectedAt: null
+      };
+    }
+    return store[userId];
   }
 
   return {
-    totalUsers: Object.keys(store).length,
-    totals: {
-      joins: totalJoins,
-      messages: totalMessages,
-      presenceReplies: totalPresence
+
+    trackJoin(userId, roomId) {
+      const u = ensure(userId);
+      u.joins += 1;
+      u.rooms[roomId] = (u.rooms[roomId] || 0) + 1;
+      u.lastActive = Date.now();
     },
-    users: store
+
+    trackMessage(userId) {
+      const u = ensure(userId);
+      u.messages += 1;
+      u.lastActive = Date.now();
+    },
+
+    trackPresenceReply(userId) {
+      const u = ensure(userId);
+      u.presenceReplies += 1;
+      u.lastActive = Date.now();
+    },
+
+    trackDisconnect(userId) {
+      const u = ensure(userId);
+      u.disconnectedAt = Date.now();
+      u.lastActive = Date.now();
+    },
+
+    getStats() {
+
+      let totalJoins = 0;
+      let totalMessages = 0;
+      let totalPresence = 0;
+
+      for (const id in store) {
+        const u = store[id];
+        totalJoins += u.joins || 0;
+        totalMessages += u.messages || 0;
+        totalPresence += u.presenceReplies || 0;
+      }
+
+      return {
+        totalUsers: Object.keys(store).length,
+        totals: {
+          joins: totalJoins,
+          messages: totalMessages,
+          presenceReplies: totalPresence
+        },
+        users: store
+      };
+    }
+
   };
 }
+
+module.exports = createAnalytics;
