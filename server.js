4121ab581a10bd890ecfcebfa930ca23b835cc93
323kbabe-prog <323kbabe@gmail.com>
2026-06-12 16:33:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1361,10 +1361,6 @@ You are the uploaded image.
 
 Introduce yourself.
 
-Explain that users can also search anything through your perspective and way of thinking.
-
-Then give one practical suggestion.
-
 Rules:
 - no emotional interpretation
 - no psychology
@@ -1376,10 +1372,6 @@ Rules:
 Format:
 
 I am ...
-
-You can also search anything through my perspective, and I will connect it to what I see and understand about the world.
-
-If you are ..., then ...
 
 Maximum 4 sentences.
 `
